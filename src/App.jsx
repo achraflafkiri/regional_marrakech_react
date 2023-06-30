6af7a7385bd94f5749faec1a1895aca7a32c6e98
ACHRAF LAFKIRI <101811681+achraflafkiri@users.noreply.github.com>
2023-06-30 01:14:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,154 +1,86 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const App = () => {
-  const [femme, setFemme] = useState("");
-  const [homme, setHomme] = useState("");
-  const [nomSociete, setNomSociete] = useState("");
-  const [titrePr, setTitrePr] = useState("");
-  const [checkSociete, setCheckSociete] = useState(false);
-  const [tel, setTel] = useState("");
-  const [disponibilite, setDisponibilite] = useState(false);
-  const [tarif, setTarif] = useState("");
-  const [competences, setCompetences] = useState([]);
+  const [deja_redoublant, setDeja_redoublant] = useState(false);
+  const [annee, setAnnee] = useState("1_eme_annee");
+  const [nombres_total_dabsences, setNombres_total_dabsences] = useState(0);
+  const [moyenne_generale, setMoyenne_generale] = useState("");
+  const [resultat, setResultat] = useState("");
 
-  const handleAjouterCompetence = (e) => {
+  const handleClick = (e) => {
     e.preventDefault();
-    const competences_maitrisees = document.getElementById(
-      "competences_maitrisees"
-    ).value;
 
-    // console.log(competences_maitrisees);
+    if (!moyenne_generale) {
+      alert("moyenne generale is required");
+    }
 
-    if (competences_maitrisees) {
-      // competences.push(competences_maitrisees);
-      // console.log(competences);
-      setCompetences([...competences, competences_maitrisees]);
+    const moyenne = parseFloat(moyenne_generale);
+    if (isNaN(moyenne)) {
+      setResultat("");
+      return;
+    }
+
+    if (moyenne >= 0 && moyenne < 10) {
+      setResultat("redoublant");
+    } else if (moyenne >= 10 && moyenne <= 20) {
+      setResultat("admin");
+    } else {
+      setResultat("");
     }
   };
 
-  const handleValider = (e) => {
-    e.preventDefault();
-  };
-
   return (
-    <div style={{ border: "1px solid #000", padding: "10px 15px" }}>
-      <form>
-        <div>
-          <div>
-            <label htmlFor="gender">Femme</label>
-            <input
-              type="radio"
-              name="gender"
-              value={femme}
-              id="gender"
-              onChange={(e) => setFemme(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="gender">Homme</label>
-            <input
-              type="radio"
-              name="gender"
-              value={homme}
-              id="gender"
-              onChange={(e) => setHomme(e.target.value)}
-            />
-          </div>
-        </div>
-        <div>
-          <label htmlFor="checkhavesociete">Oui, je suis une société.</label>
-          <input
-            type="checkbox"
-            name="checkhavesociete"
-            checked={checkSociete}
-            onChange={(e) => setCheckSociete(e.target.checked)}
-          />
-        </div>
-        {checkSociete && (
-          <div>
-            <label htmlFor="nomSociete">Nom de Société:</label>
-            <input
-              type="text"
-              name="nom"
-              value={nomSociete}
-              id="nomSociete"
-              onChange={(e) => setNomSociete(e.target.value)}
-              placeholder="Nom de Société"
-            />
-          </div>
-        )}
-        <div>
-          <label htmlFor="titrePr">Titre Profile*:</label>
-          <input
-            type="text"
-            name="titrePr"
-            value={titrePr}
-            id="titrePr"
-            onChange={(e) => setTitrePr(e.target.value)}
-            placeholder="Titre Profile"
-          />
-        </div>
-        <div>
-          <label htmlFor="paysVille">Pays/Ville *:</label>
-          <select name="paysVille" id="paysVille">
-            <option value="maroc" defaultValue>
-              Maroc
-            </option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="tel">
-            Téléphone *: <small>Confidential</small>:
-          </label>
-          <input
-            type="text"
-            name="tel"
-            id="tel"
-            value={tel}
-            onChange={(e) => setTel(e.target.value)}
-            placeholder="123456789"
-          />
-        </div>
-        <hr />
-        <div>
-          <label htmlFor="disponibilite">Disponibilité *:</label>
-          <input
-            type="checkbox"
-            name="disponibilite"
-            checked={disponibilite}
-            onChange={(e) => setDisponibilite(e.target.checked)}
-          />
-          (A distance uniquement)
-        </div>
-        <div>
-          <label htmlFor="tarif">Tarif *:</label>
-          <input
-            type="text"
-            name="tarif"
-            id="tarif"
-            value={tarif}
-            onChange={(e) => setTarif(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="competences_maitrisees">
-            Compétences maîtrisées:
-          </label>
-          <select name="competences_maitrisees" id="competences_maitrisees">
-            <option value="" defaultValue>
-              Please select an option
-            </option>
-            <option value="senior">Senior</option>
-            <option value="debutant">Débutant</option>
-          </select>
-          <button onClick={handleAjouterCompetence}>Ajouter</button>
-        </div>
-        <div>
-          <p>Liste de vos compétences:</p>
-          <textarea rows="4" value={competences.join(", ")} readOnly />
-        </div>
-        <button onClick={handleValider}>Valider</button>
-      </form>
+    <div style={{ border: "1px solid #000", padding: "20px 15px" }}>
+      <div>
+        <label htmlFor="annee">Année</label>
+        <select
+          name="annee"
+          id="annee"
+          value={annee}
+          onChange={(e) => setAnnee(e.target.value)}
+        >
+          <option value="1_eme_annee">1ère Année</option>
+          <option value="2_eme_annee">2ème Année</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="deja_redoublant">Déjà redoublant : </label>
+        <input
+          type="checkbox"
+          name="deja_redoublant"
+          id="deja_redoublant"
+          checked={deja_redoublant}
+          onChange={(e) => setDeja_redoublant(e.target.checked)}
+        />
+      </div>
+      <div>
+        <label htmlFor="moyenne_generale">Moyenne générale : </label>
+        <input
+          type="number"
+          name="moyenne_generale"
+          value={moyenne_generale}
+          onChange={(e) => setMoyenne_generale(e.target.value)}
+          id="moyenne_generale"
+        />
+      </div>
+      <div>
+        <label htmlFor="Nombres_total_dabsences">
+          Nombres total d'absences :{" "}
+        </label>
+        <input
+          type="number"
+          name="Nombres_total_dabsences"
+          value={nombres_total_dabsences}
+          id="Nombres_total_dabsences"
+          onChange={(e) => setNombres_total_dabsences(e.target.value)}
+        />
+      </div>
+      <div>
+        <p> -----Décision : {resultat}</p>
+      </div>
+      <button type="submit" onClick={handleClick}>
+        OK
+      </button>
     </div>
   );
 };
